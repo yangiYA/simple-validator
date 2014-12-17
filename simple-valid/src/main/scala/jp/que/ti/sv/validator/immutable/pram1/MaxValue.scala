5151aa1e1ae0fd9dc4e8c7dc39be66a9d8e59e8c
yangiYA <git@ti.que.jp>
2014-12-17 20:34:17 +0900

    色々な押したのでとりあえず全部プッシュして、ベースラインにする

--- simple-valid/src/main/scala/jp/que/ti/sv/validator/immutable/pram1/MaxValue.scala
+++ simple-valid/src/main/scala/jp/que/ti/sv/validator/immutable/pram1/MaxValue.scala
@@ -4,28 +4,33 @@ import jp.que.ti.sv.NotRequiredBase
 import jp.que.ti.sv.ParameterInfo
 
 /**
- * パラメータの文字列長が、指定の長さ以下であることをチェックする Validator
+ * パラメータの数値が、指定の数値以下であることをチェックする Validator。
+ * 数字でないパラメータが指定された場合は、エラーにしません。
+ * エラーにする場合は、[[jp.que.ti.sv.validator.immutable.pram1.NumberValidator]] と組み合わせて使用してください。
  */
-class MaxValue protected (max: Int) extends MaxValueBase[Int](max) {
-  def isValidInputed(paramValue: String): Boolean = { max >= trim(paramValue).toLong }
-}
-
-class MaxValueLong protected (max: Long) extends MaxValueBase[Long](max) {
-  def isValidInputed(paramValue: String): Boolean = { max >= trim(paramValue).toLong }
-}
-
-protected class MaxValueBase[A <: { def toLong: Long; def toInt: Int; }] protected (val max: A)
-  extends NotRequiredBase("maxvalue") {
-
-  //  def isValidInputed(paramValue: String): Boolean = { max.toInt >= trim(paramValue).toLong }
-
+abstract sealed class MaxValue[A <: AnyVal] protected (val max: A) extends NotRequiredBase("maxvalue") {
   override def messageArgs(parameterInfo: ParameterInfo): Seq[String] = parameterInfo.nam4Msg :: max + "" :: Nil
+}
 
+class MaxValueLong(max: Long) extends MaxValue[Long](max) {
+  def isValidInputed(paramValue: String): Boolean = try {
+    max >= trim(paramValue).toLong
+  } catch {
+    case e: Exception => true // 数字でない場合はエラーににしない
+  }
+}
+
+class MaxValueInt(max: Int) extends MaxValue[Int](max) {
+  def isValidInputed(paramValue: String): Boolean = try {
+    max >= trim(paramValue).toLong
+  } catch {
+    case e: Exception => true // 数字でない場合はエラーににしない
+  }
 }
 
 object MaxValue {
-  def apply(max: Int) = new MaxValue(max)
-  def apply(max: Long) = new MaxValueLong(max)
+  def apply(max: Int): MaxValue[Int] = new MaxValueInt(max)
+  def apply(max: Long): MaxValue[Long] = new MaxValueLong(max)
 
 }
 
